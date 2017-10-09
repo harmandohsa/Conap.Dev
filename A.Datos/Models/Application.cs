8014b20a9493db9906e9d759b42a19dba61ef3e5
Hector Armando Hernandez Samayoa <hhernandez@query.com.gt>
2017-10-09 10:52:23 -0600

    09102017HH

--- A.Datos/Models/Application.cs
+++ A.Datos/Models/Application.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,8 @@ namespace A.Datos.Models
     {
         [Key]
         public int ApplicationId { get; set; }
+        [Required]
+        [DisplayName("Sistema")]
         public string ApplicationName { get; set; }
     }
 }
