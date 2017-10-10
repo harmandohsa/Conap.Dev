818fa6a54a344c90fa2048188de0a9b7d6d451b2
Hector Armando Hernandez Samayoa <hhernandez@query.com.gt>
2017-10-09 22:34:27 -0600

    09102017HH.2

---
+++ A.Datos/Models/Page.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace A.Datos.Models
+{
+    public class Page
+    {
+        [Key]
+        public int PageId { get; set; }
+        [Required]
+        [Display(Name = "Página")]
+        public string PageName { get; set; }
+        public int ApplicationId { get; set; }
+        public virtual Application Application { get; set; }
+
+    }
+}
---
+++ A.Datos/Models/User_Type.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace A.Datos.Models
+{
+    public class User_Type
+    {
+        [Key]
+        public int User_TypeId { get; set; }
+        [Required]
+        [Display(Name = "Tipo de Usuario")]
+        public string User_TypeName { get; set; }
+
+        public int ApplicationId { get; set; }
+
+        public virtual Application Application { get; set; }
+    }
+}
