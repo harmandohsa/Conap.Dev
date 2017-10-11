aad437ec6c77bc138042d451cd758f2c195acf76
Hector Armando Hernandez Samayoa <hhernandez@query.com.gt>
2017-10-11 15:12:37 -0600

    11102017HH

--- A.Datos/Context/ConapContext.cs
+++ A.Datos/Context/ConapContext.cs
@@ -11,5 +11,9 @@ namespace A.Datos.Context
     public class ConapContext : DbContext
     {
         public DbSet<Application> Applications { get; set; }
+
+        public System.Data.Entity.DbSet<A.Datos.Models.Page> Pages { get; set; }
+
+        public System.Data.Entity.DbSet<A.Datos.Models.User_Type> User_Type { get; set; }
     }
 }
