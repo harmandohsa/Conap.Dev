3b9070461d020fa229952b83d99f98daf0b93c42
Hector Armando Hernandez Samayoa <hhernandez@query.com.gt>
2017-10-11 15:07:50 -0600

    11102017HH

---
+++ Adm.Usuarios/Controllers/PagesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using A.Datos.Context;
+using A.Datos.Models;
+
+namespace Adm.Usuarios.Controllers
+{
+    public class PagesController : Controller
+    {
+        private ConapContext db = new ConapContext();
+
+        // GET: /Pages/
+        public ActionResult Index()
+        {
+            var pages = db.Pages.Include(p => p.Application);
+            return View(pages.ToList());
+        }
+
+        // GET: /Pages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Page page = db.Pages.Find(id);
+            if (page == null)
+            {
+                return HttpNotFound();
+            }
+            return View(page);
+        }
+
+        // GET: /Pages/Create
+        public ActionResult Create()
+        {
+            ViewBag.ApplicationId = new SelectList(db.Applications, "ApplicationId", "ApplicationName");
+            return View();
+        }
+
+        // POST: /Pages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="PageId,PageName,ApplicationId")] Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pages.Add(page);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ApplicationId = new SelectList(db.Applications, "ApplicationId", "ApplicationName", page.ApplicationId);
+            return View(page);
+        }
+
+        // GET: /Pages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Page page = db.Pages.Find(id);
+            if (page == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ApplicationId = new SelectList(db.Applications, "ApplicationId", "ApplicationName", page.ApplicationId);
+            return View(page);
+        }
+
+        // POST: /Pages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="PageId,PageName,ApplicationId")] Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(page).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ApplicationId = new SelectList(db.Applications, "ApplicationId", "ApplicationName", page.ApplicationId);
+            return View(page);
+        }
+
+        // GET: /Pages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Page page = db.Pages.Find(id);
+            if (page == null)
+            {
+                return HttpNotFound();
+            }
+            return View(page);
+        }
+
+        // POST: /Pages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Page page = db.Pages.Find(id);
+            db.Pages.Remove(page);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
