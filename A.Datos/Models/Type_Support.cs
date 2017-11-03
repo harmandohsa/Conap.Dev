b9e222da71df64d0178dc168dcb6d3b3bb93756c
ptmej <ptmejia2312@hotmail.com>
2017-11-03 11:39:43 +0100

    03112017PM

---
+++ A.Datos/Models/Type_Support.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace A.Datos.Models
+{
+    public class Type_Support
+
+    {
+        [Key]
+        public int TypesupportId { get; set; }
+        [Required]
+        [Display(Name = "Tipo de Soporte")]
+        public string TypesupportName { get; set; }
+    }
+}
